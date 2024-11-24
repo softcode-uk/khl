99905ee4ef93df2387d0b5808fafc33a7d860805
Layik Hama <layik.hama@gmail.com>
2024-11-24 10:21:37 +0000

    set site url for gh tests

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://softcode-uk.github.io',
+  site: 'https://softcode-uk.github.io/khl',
   base: 'khl',
   output: 'static',
 
