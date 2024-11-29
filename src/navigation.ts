11fd9e8dfd20cc16ee5648dd49802e60e199014d
Layik Hama <layik.hama@gmail.com>
2024-11-29 23:33:35 +0000

    nav links

--- src/navigation.ts
+++ src/navigation.ts
@@ -80,6 +80,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Using <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · <a class="text-blue-600 underline dark:text-muted" href='www.softcode.co.uk'>www.softcode.co.uk</a> · Open Source www.github.com/softcode/khl
+    Using <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · <a class="text-blue-600 underline dark:text-muted" href="https://softcode.co.uk">www.softcode.co.uk</a> · Open Source <a class="text-blue-600 underline dark:text-muted" href="www.github.com/softcode-uk/khl">github.com/softcode-uk/khl</a>
   `,
 };
